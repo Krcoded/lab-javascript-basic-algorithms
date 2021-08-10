b0469b0072347277b926b4ca43d6dd94d9af6620
Kevin Rodriguez <87106659+Krcoded@users.noreply.github.com>
2021-08-10 13:57:56 -0400

    first lab

--- js/index.js
+++ js/index.js
@@ -1,7 +1,57 @@
-// Iteration 1: Names and Input
+console.log("hello world");
+
+console.log("I am ready!");
 
 
-// Iteration 2: Conditionals
+
+var navigatorName = "Joe"
+
+console.log(`The Navigators name is ${navigatorName}`)
 
 
-// Iteration 3: Loops
+var driverName = "Kevin"
+
+console.log(`The drivers name is ${driverName}`)
+
+
+
+if (driverName.length > navigatorName.length)
+    console.log(`The driver has the longest name, it has ${driverName.length} characters.`)
+
+
+ else if (navigatorName.length > driverName.length)
+      console.log(`It seems that the navigator has the longest name, it has ${navigatorName.length} characters.`)
+
+
+     else console.log(`Wow, you both have equally long names,  characters!`)
+
+
+
+
+console.log(  driverName.split('').join(' ').toUpperCase(''))
+
+
+let navigatorReverse = []
+for(let i=navigatorName.length-1; i >=0; i--){
+  navigatorReverse.push(navigatorName[i])
+}
+
+console.log( navigatorReverse.join('') )
+
+
+
+let names = [driverName, navigatorName];
+console.log(names)
+
+names.sort();
+console.log(names)
+
+
+if (driverName.localeCompare(navigatorName) === -1){
+  console.log(`The ${driverName} name goes first.`)
+}
+else if ( driverName.localeCompare(navigatorName) === 1) {
+  console.log(`Yo, the ${navigatorName} goes first definitely.`) 
+}
+else {
+  console.log(`What?! You both have the same name?`) }
